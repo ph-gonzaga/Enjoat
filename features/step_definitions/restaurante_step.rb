e18507abf6e92d941ca7d6bcb075da8b68bff128
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-11 13:58:05 -0300

    Update restaurante step
    Inclusao de comportamento nas steps

--- features/step_definitions/restaurante_step.rb
+++ features/step_definitions/restaurante_step.rb
@@ -1,21 +1,43 @@
+Given("que tenho uma lista de restaurantes") do
+    @restaurant_data = [
+        { nome: "Bread & Bakery", category: "Padaria", delivery_time: "25 minutos", rating: 4.9 },
+        { nome: "Burguer House", category: "Hamburgers", delivery_time: "30 minutos", rating: 3.5 },
+      ]
+end
+
 When("acesso a lista de restaurantes") do
   visit "/restaurants"
+
+  
 end
 
 Then("vejo todas as opções disponíveis") do
   restaurantes = all(".restaurant-item")
+  expect(restaurantes.size).to be > 0
   puts restaurantes.size
   puts restaurantes.class
 end
 
 Then("cada restaurante deve exibir sua categoria") do
-  pending # Write code here that turns the phrase above into concrete actions
+  restaurants = all(".restaurant-item")
+  @restaurant_data.each_with_index do |value, index|
+    puts value[:category]
+    expect(restaurants[index]).to have_text value[:category]
+  end
 end
 
 Then("cada restaurante deve exibir o tempo de entrega") do
-  pending # Write code here that turns the phrase above into concrete actions
+  restaurants = all(".restaurant-item")
+  @restaurant_data.each_with_index do |value, index|
+    puts value[:category]
+    expect(restaurants[index]).to have_text value[:delivery_time]
+  end
 end
 
 Then("cada restaurante deve exibir sua nota de avaliação") do
-  pending # Write code here that turns the phrase above into concrete actions
+  restaurants = all(".restaurant-item")
+  @restaurant_data.each_with_index do |value, index|
+    puts value[:category]
+    expect(restaurants[index]).to have_text value[:rating]
+  end
 end
