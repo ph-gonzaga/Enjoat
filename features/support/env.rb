e35b06678b73752f708bdfa38d2ea4dc2e92d5b4
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-11 10:28:40 -0300

    Update env.rb
    Inclusao de configuracao capybara e inclusao de dependencias

---
+++ features/support/env.rb
@@ -0,0 +1,13 @@
+require "capybara"
+require "capybara/cucumber"
+require "selenium-webdriver"
+
+require "capybara"
+require "capybara/cucumber"
+require "selenium-webdriver"
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.default_max_wait_time = 10
+  config.app_host = "https://enjoeat-sp1.herokuapp.com"
+end
