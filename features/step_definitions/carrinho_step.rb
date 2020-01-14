289271b7e28433bdef066daf2b2130fe1237b02f
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-14 16:41:28 -0300

    Update carrinho_step
    -Inclusao do times

--- features/step_definitions/carrinho_step.rb
+++ features/step_definitions/carrinho_step.rb
@@ -6,11 +6,13 @@ Given("o valor do produto é de {string}") do |valor|
   @produto_valor = valor
 end
 
-When("eu adiciono {int} unidade") do |int|
-  find(".menu-item-info-box", text: @produto_nome.upcase).find(".add-to-cart").click
+When("eu adiciono {int} unidade\\(s)") do |quantidade|
+  quantidade.times do
+    find(".menu-item-info-box", text: @produto_nome.upcase).find(".add-to-cart").click
+  end
 end
 
-Then("{int} unidade deste item deve ser adicionado ao carrinho") do |quantidade|
+Then("{int} unidade\\(s) deste item devem ser adicionados ao carrinho") do |quantidade|
   cart = find("#cart")
   expect(cart).to have_text "(#{quantidade}x) #{@produto_nome}"
   puts "(#{quantidade}x) #{@produto_nome}"
