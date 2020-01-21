63e1c3d27fa018ad09df2e8acc8b85152bc39dd3
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-21 13:57:48 -0300

    Update carrinho_step
    Encapsulamento

--- features/step_definitions/carrinho_step.rb
+++ features/step_definitions/carrinho_step.rb
@@ -13,15 +13,12 @@ When("eu adiciono {int} unidade\\(s)") do |quantidade|
 end
 
 Then("{int} unidade\\(s) deste item deve ser adicionados ao carrinho") do |quantidade|
-  cart = find("#cart")
-  expect(cart).to have_text "(#{quantidade}x) #{@produto_nome}"
+  expect(@cart_page.box).to have_text "(#{quantidade}x) #{@produto_nome}"
   puts "(#{quantidade}x) #{@produto_nome}"
 end
 
 Then("o valor total deve ser de {string}") do |valor_total|
-  cart = find("#cart")
-  total = cart.find("tr", text: "Total:").find("td")
-  expect(total.text).to have_text valor_total
+  expect(@cart.total.text).to have_text valor_total
 end
 
 #lista de produtos
@@ -39,10 +36,9 @@ When("eu adiciono todos os itens") do
 end
 
 Then("vejo todos os itens no carrinho") do
-  cart = find("#cart")
   quantidade = 1
   @product_list.each do |p|
-    expect(cart).to have_text "(#{p["quantidade"]}x) #{p["nome"]}"
+    expect(@cart_page.box).to have_text "(#{p["quantidade"]}x) #{p["nome"]}"
   end
 end
 
@@ -50,27 +46,23 @@ end
 
 Given("que eu tenho itens no carrinho:") do |table|
   @product_list = table.hashes
-
   steps %{
     When eu adiciono todos os itens
   }
 end
 
 When("eu removo somente o item {int}") do |item|
-  cart = find("#cart")
-  cart.all("table tbody tr")[item].find(".danger").click
+  @cart_page.remove_item(item)
 end
 
 When("eu removo todos os itens") do
   @product_list.each_with_index do |value, index|
-    cart = find("#cart")
-    cart.all("table tbody tr")[index].find(".danger").click
+    @cart_page.remove_item(index)
   end
 end
 
 Then("vejo a seguinte mensagem no carrinho {string}") do |mensagem|
-  cart = find("#cart")
-  expect(cart).to have_text mensagem
+  expect(@cart_page.box).to have_text mensagem
 end
 
 #limpar carrinho
