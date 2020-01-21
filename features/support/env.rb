c8342471996acbd5bc853a1ee2004140b1495726
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-21 13:56:47 -0300

    Ajuste env.rb e hooks.rb
    - Novo ambiente SP4
    - Variavel de instancia para page cart.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,5 +9,5 @@ require "selenium-webdriver"
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.default_max_wait_time = 10
-  config.app_host = "https://enjoeat-sp3.herokuapp.com"
+  config.app_host = "https://enjoeat-sp4.herokuapp.com"
 end
